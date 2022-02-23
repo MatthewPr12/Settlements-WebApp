1e66ae212935724cd006743908dfd2ee265b4165
Oleg Omelchuk <54985253+qLate@users.noreply.github.com>
2022-02-23 10:59:51 +0200

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from ImageTextReader import image_to_text
-
-import os
-
-for dir_name in os.listdir("images"):
-    dir_full = os.path.join("images", dir_name)
-    for img_name in os.listdir(dir_full):
-        image_to_text(os.path.join(dir_full, img_name),
-                      os.path.join("image_texts", img_name.rstrip(".jpeg")+"_text.txt"))

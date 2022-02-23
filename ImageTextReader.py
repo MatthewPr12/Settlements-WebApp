60c362a5a20f12590ccb64774353cfeaa860e7df
qLate <uaolegikua@gmail.com>
2022-02-23 10:34:38 +0200

    implemented image text reading

--- ImageTextReader.py
+++ ImageTextReader.py
@@ -2,9 +2,11 @@ import pytesseract
 import cv2
 
 
-def read_image(image):
-    img = cv2.imread(image)
+def image_to_text(read_path, save_path):
+    img = cv2.imread(read_path)
 
-    text = pytesseract.image_to_string(img, lang="ukr")
-    with open(image, 'w', encoding='utf-8') as file:
+    tessdata_dir_config = r'--tessdata-dir "/home/oleg/share/tessdata"'
+    text = pytesseract.image_to_string(
+        img, lang="ukr", config=tessdata_dir_config)
+    with open(save_path, 'w', encoding='utf-8') as file:
         file.write(text)
